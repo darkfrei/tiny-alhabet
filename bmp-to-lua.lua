2291bd54a8cce06a6ef75f4a2e2426f2f15f7ba5
darkfrei <34188677+darkfrei@users.noreply.github.com>
2020-03-29 23:27:14 +0200

    Update bmp-to-lua.lua

--- bmp-to-lua.lua
+++ bmp-to-lua.lua
@@ -1,25 +1,25 @@
-local File = assert(io.open('D:/Lua/ZBS/myprograms/my/a.bmp'))
+local File = assert(io.open('D:/Lua/ZBS/myprograms/my/c.bmp', 'rb'))
 local bytecode = File:read("*all")
+--local bytecode = File:read("*a")
+--File:close()
 local size = #bytecode
+print ("size: " .. size)
 
---[[
+-----------------
 local bytes = {}
+local str = ""
 for i = 1, size do
 	local byte = bytecode:byte(i)
+	str = str .. ' ' .. byte
 	table.insert (bytes, byte)
 end
-local str = ""
-for i, v in pairs (bytes) do
-	str = str .. ' ' .. v
-end
 print (str) 
-]]
-
-print (("size: " .. size))
+-----------------
 
 local bmp_structure = { 
 	typ = {offset = 1, length = 2}, -- returns 19778, magic value for BMP-format
-	off_bits = {offset = 11, length = 2}, -- returns 54 bytes, amount of system bytes
+--	off_bytes = {offset = 11, length = 2}, -- returns 54 bytes, amount of system bytes
+	off_bytes = {offset = 11, length = 1}, -- returns 54 bytes, amount of system bytes
 	width = {offset = 19, length = 4}, -- returns width in pixels without last black pixel
 	height = {offset = 23, length = 4}, -- returns height in pixels
 	bit_count = {offset = 29, length = 2}, -- must be 24 bits per color; one byte for subpixel
@@ -46,64 +46,54 @@ for name, tabl in pairs (bmp_structure) do
 	tabl.value = value
 end
 
-local offset_data = bmp_structure.off_bits.value + 1
+local offset_data = bmp_structure.off_bytes.value + 1
 
 print ('offset_data: ' .. offset_data)
 local bmp_data_x = {}
 local bmp_data_y = {}
-local n = 1
-local width = bmp_structure.width.value + 1 -- last hidden pixel is black
+local n = 0
+local width = bmp_structure.width.value
 local height = bmp_structure.height.value
+local n_pixels = width * height
+print ('n_pixels:' .. n_pixels)
 local x = 1
-local y = height
-for i = offset_data, size, 3 do
-	
-	local pixel = {}
-	local str = ''
-	for j, sp in pairs ({'r', 'g', 'b'}) do
-		local byte = bytecode:byte(i+3-j) -- wow
-		pixel[sp] = byte -- sp is subpixel
-		str = str .. sp  .. ':' .. byte .. ' '
-	end
-	if not bmp_data_x[x] then bmp_data_x[x] = {} end
-	if not bmp_data_y[y] then bmp_data_y[y] = {} end
-	if not (x == width) then -- without last black pixel 
---		print ('x:' .. x ..  ' y:' .. y .. ' pixel:' .. str)
-		bmp_data_x[x][y] = pixel
-		bmp_data_y[y][x] = pixel
-	end
-	x = x + 1
-	if x > width then
-		x = 1
-		y = y-1
-	end
-	n=n+1
-end
-print ('n:' .. n)
+--local y = height
+local y = 1
+local bit_count = bmp_structure.bit_count.value
 
-
---for x, ys in pairs (bmp_data_x) do
---	for y, pixel in pairs (ys) do
---		local str = ''
---		for i, sp in pairs ({'r', 'g', 'b'}) do
---			local value = pixel[sp]
---			str = str .. sp .. ':' .. value .. ' '
---		end
---		print ('x:' .. x .. ' y:' .. y .. ' color: ' .. str)
---	end
---end
-
-for y, xs in pairs (bmp_data_y) do
-	for x, pixel in pairs (xs) do
+function get_24_bit_data (bytecode, offset_data, size, width, height)
+	local pixels = {}
+	local x = 1
+	local y = height
+	for i = offset_data, size, 3 do
+		local pixel = {}
 		local str = ''
-		for i, sp in pairs ({'r', 'g', 'b'}) do
-			local value = pixel[sp]
-			str = str .. sp .. ':' .. value .. ' '
+		for j, sp in pairs ({'r', 'g', 'b'}) do
+			local byte = bytecode:byte(i+3-j) or 0 -- wow
+			pixel[sp] = byte -- sp is subpixel
+			str = str .. sp  .. ':' .. byte .. ' '
 		end
-		print ('x:' .. x .. ' y:' .. y .. ' color: ' .. str)
+		pixel.x = x
+		pixel.y = y
+		if y > 0 then
+			table.insert (pixels, pixel)
+			print ('x:' .. x .. ' y:' .. y .. ' str: ' .. str)
+			x = x + 1
+			if x > width then
+				x = 1
+				y = y-1
+			end
+		end
 	end
+	print ('#pixels:' .. #pixels)
+	return pixels
 end
 
-
-
-
+if bit_count == 24 then
+	print ('24 bit color')
+	bmp_data_x = get_24_bit_data (bytecode, offset_data, size, width, height)
+	
+elseif bit_count == 8 then
+	print ('8 bit color')
+	
+end
